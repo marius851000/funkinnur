03870ea5a7c0d3337ef33816389e31b33e251ad3
marius david <marius@mariusdavid.fr>
2024-08-11 13:56:50 +0200

    hydra check

--- flake.nix
+++ flake.nix
@@ -6,10 +6,15 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
         packages = import ./test.nix { inherit pkgs; };
+      in
+      rec {
+        inherit packages;
         defaultPackage = packages.smoke-em-out-struggle;
-      }
+      } // (if system == "x86_64" then {
+        hydraJobs = packages;
+      } else {})
     );
 }
